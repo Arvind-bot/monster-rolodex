30bada423a5a7986b312a2da2c9f1a08cb65a2c8
Arvind Kumar <arvindof7@gmail.com>
2020-09-04 18:37:52 +0530

    Updates

---
+++ precache-manifest.d90a41a7e5dee47875fd1fb2975e39d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5beb0720f00958c86a1d201f6d36cdd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ca91a19e51e289301e44",
+    "url": "/monster-rolodex/static/css/main.a6bfaee6.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca91a19e51e289301e44",
+    "url": "/monster-rolodex/static/js/main.a231fa35.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
